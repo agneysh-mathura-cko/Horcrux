da1948dc82540a5ff52c71cc385b5cdf2aa8f781
Agneysh-Mathura-CKO <agneysh.mathura@checkout.com>
2021-04-29 02:16:27 +0400

    feat: Wallet create transaction

--- wallet/index.js
+++ wallet/index.js
@@ -1,3 +1,4 @@
+const Transaction = require('./transaction'); 
 const { STARTING_BALANCE } = require('../config');
 const { ec, cryptoHash } = require('../util');
 
@@ -10,9 +11,16 @@ class Wallet {
         this.publicKey = this.keyPair.getPublic().encode('hex');
     }
 
-    sign(data){
+    sign(data) {
         return this.keyPair.sign(cryptoHash(data));
     }
+
+    createTransaction({ recipient, amount }) {
+        if (amount > this.balance) {
+          throw new Error('Amount exceeds balance');
+        }
+        return new Transaction({ senderWallet: this, recipient, amount });
+      }
 }
 
 module.exports = Wallet;
